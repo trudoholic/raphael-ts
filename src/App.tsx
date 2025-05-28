a3131132668df7cd04bf85c94257218f6ce9f757
Oleg <35769958+trudoholic@users.noreply.github.com>
2025-05-28 13:14:13 +0300

    paper

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useEffect, useState} from 'react'
 import './App.css'
+import Raphael from "raphael"
 
 function App() {
   const [count, setCount] = useState(0)
 
+  useEffect(() => {
+    const paper = new Raphael(0, 0, 500, 500)
+    // const paper = new Raphael(document.getElementById('canvas_container'), 500, 500)
+
+    // const circle = paper.circle(100, 100, 80)
+    for (let i = 0; i < 5; ++i) {
+      const k = i * 5
+      paper.circle(250 + (2 * k), 100 + k, 50 - k)
+    }
+    const rectangle = paper.rect(200, 200, 250, 100)
+    rectangle.attr({
+      fill: '90-#526c7a-#64a0c1',
+      stroke: '#3b4449', 'stroke-width': 10, 'stroke-linejoin': 'round',
+      transform: 'r-90',
+    })
+
+    const ellipse = paper.ellipse(200, 400, 100, 50)
+
+    const tetromino = paper.path("M 250 250 l 0 -50 l -50 0 l 0 -50 l -50 0 l 0 50 l -50 0 l 0 50 z")
+    tetromino.attr({
+      fill: '#9cf', 'fill-opacity': 0.5,
+      stroke: '#ddd', 'stroke-width': 5,
+    })
+
+    /*/
+    tetromino.animate(
+      {
+        transform: 'r360',
+        'stroke-width': 1,
+      }, 2000, 'bounce',
+      function() {
+        console.log('callback after original animation finishes')
+        this.animate({
+          transform: 'r-90',
+          stroke: '#3b4449',
+          'stroke-width': 10
+        }, 1000);
+      }
+    )
+    /*/
+
+    // console.log(tetromino)
+  }, [])
+
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <div
+        id="canvas_container"
+        style={{
+          width: "500px",
+          height: "50px",
+          border: "1px solid #aaa",
+        }}
+      />
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
